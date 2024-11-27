641ab472814061c89bd5a090e057d9c726fd31c0
Spriha Shringi <shringispriha@gmail.com>
2024-11-27 22:14:47 +0530

    creating tasks

--- src/components/TaskList/CompletedTask.jsx
+++ src/components/TaskList/CompletedTask.jsx
@@ -2,18 +2,18 @@ import React from 'react'
 
 const CompletedTask = () => {
   return (
-    <div className='flex-shrink-0 h-full w-[300px] p-5 bg-red-400 rounded-xl'>
+    <div className='flex-shrink-0 h-full w-[300px] p-5 bg-blue-400 rounded-xl'>
        <div className='flex justify-between items-center'>
-        <h3 className='bg-green-600 text-px-3 py-1 rounded'>High</h3>
-        <h4 className='text-sm'>18 Nov 2024</h4>
+        <h3 className='bg-red-600 text-px-3 py-1 rounded'>{data.category} </h3>
+        <h4 className='text-sm'>{data.date}</h4>
       </div>
-    <h2 className='mt-5 text-2xl font-semibold'>BBCN</h2>
-    <p className='text-sm mt-2'>Quiz-2</p>
-    <p className='text-sm mt-2'>Paper select and ask sir</p>
+    <h2 className='mt-5 text-2xl font-semiboldrounded font-medium'>{data.taskTitle}</h2>
+    <p className='text-sm mt-2'>{data.taskDescription}</p>
+    {/* <p className='text-sm mt-2'>Paper select and ask sir</p> */}
     <div className='flex justify-between mt-4'> 
         {/* //     ''/"" */}
-        <button className='bg-green-500 py-1 px-2 text-sm'>Mark as Completed</button>
-        <button className='bg-red-500 py-1 px-2 text-sm'>Mark as Failed</button>
+        <button className='bg-green-500 py-1 px-2 text-sm'>Completed</button>
+        {/* <button className='bg-red-500 py-1 px-2 text-sm'>Mark as Failed</button> */}
     </div>
     </div>
   )
